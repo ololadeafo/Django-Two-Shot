408e066e2c755a0ade5bb06a31956d9b2eedb649
lola <lola.afolabi0503@gmail.com>
2023-01-19 14:22:16 -0600

    Feature 5 complete

--- expenses/urls.py
+++ expenses/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def redirect_to_home(request):
+    return redirect("home")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", redirect_to_home),
+    path("receipts/", include("receipts.urls")),
 ]
